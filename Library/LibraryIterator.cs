d25b712aad6dd5b1ae74eb4e3c7cd0f35deb6a31
Martin-Minev <72812000+Martin-Minev@users.noreply.github.com>
2021-03-17 20:41:37 +0100

    1st

---
+++ Library/LibraryIterator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class LibraryIterator<Book> : IEnumerator<Book>
+    {
+        private List<Book> books;
+
+        private int currentIndex;
+
+        public LibraryIterator(List<Book> books)
+        {
+            Reset();
+            this.books = books;
+        }
+
+        public Book Current => books[currentIndex];
+
+        object IEnumerator.Current => this.Current;
+
+        public void Dispose()
+        {
+            this.books = null;
+        }
+
+        public bool MoveNext()
+        {
+            currentIndex++;
+            return currentIndex < books.Count;
+        }
+
+        public void Reset()
+        {
+            currentIndex = -1;
+        }
+    }
+}
